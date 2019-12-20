5dc851baa66d0ae7fd5c27d9f16831b4c9b8efcd
prasadhmurugan <prasadhmurugan1988@gmail.com>
2019-12-20 10:40:23 +0530

    finished all find bugs

---
+++ src/config/rest_consfig.js
@@ -0,0 +1,4 @@
+export const articles_url = 'https://newsapi.org/v2/top-headlines';
+export const country_code = 'in';
+export const category = 'general';
+export const _api_key = '9b3a826b03534f848c8f380a26aedbb0';
